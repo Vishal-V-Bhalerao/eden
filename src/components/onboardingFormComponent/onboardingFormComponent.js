e24f0664f057ecaa067d3791fbd34717c250072a
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2022-05-05 09:09:51 +0530

    initial setup

---
+++ src/components/onboardingFormComponent/onboardingFormComponent.js
@@ -0,0 +1,12 @@
+import './index.css'
+export default function OnboardingFormComponent() {
+    return (
+        <div className="form-parent">
+            <div className='form-div flex-column' >
+                <div>
+                    <img src='./icons/eden.PNG' alt="logo" ></img>
+                </div>
+            </div>
+        </div>
+    )
+}
